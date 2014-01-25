e572898538c4c0d4baeaa4be3b8558ea9130757b
Trent Small <sixstring982@gmail.com>
2014-01-25 11:45:00 -0700

    Added basic console ticking.

---
+++ src/console.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "console.h"
+
+bool Console_DataAvailable()
+{
+  fd_set rfds;
+  struct timeval tv;
+  int retval;
+
+  FD_ZERO(&rfds);
+  FD_SET(0, &rfds);
+
+  tv.tv_sec = TICK_SECS;
+  tv.tv_usec = 0;
+
+  retval = select(1, &rfds, NULL, NULL, &tv);
+
+  switch(retval)
+  {
+  case -1: perror("IOError in select!\n"); break;
+  case 0:  return false;
+  default:  return true;
+  }
+}
+
+void Console_ReadLine(char* toBuffer)
+{
+  int readC;
+  s32 i;
+  for(i = 0; i < MAX_BUFFER_LENGTH; i++)
+  {
+    switch((readC = fgetc(stdin)))
+    {
+    case '\n': toBuffer[i] = 0; return;
+    default: toBuffer[i] = readC; break;
+    }
+  }
+}

ccdc9471c09cf67e80a75439eb190118ec1a304e
l3acon <l3acon@gmail.com>
2014-01-25 15:04:14 -0700

    added d.h

---
+++ include/departments.h
@@ -0,0 +1,11 @@
+#include "command.h"
+
+#define COMMANDLIMIT 32
+
+typedef struct s_departmentCommands
+{
+	int commandspace;  				/*total number of department commands */
+	char * departmentname;				/*department name */
+	char *commands[COMMANDLIMIT];		/*list of commands */
+}Department;
+

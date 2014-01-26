a541d6045b6de3a4b30ccc6c49400afba119c2ca
l3acon <l3acon@gmail.com>
2014-01-25 20:57:14 -0700

    added magenta sonar

--- src/sonar.c
+++ src/sonar.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "sonar.h"
 
+#define COLOR_MAGENTA "\033[35m"      /* Magenta */
+#define COLOR_RESET   "\033[0m"
+
+
 void Sonar_PrintContacts(GameState* state)
 {
-  printf("Print Contacts!\n");
+  printf(COLOR_MAGENTA "Print Contacts!" COLOR_RESET "\n");
+
 }

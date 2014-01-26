1982919d744f0f3d2a17a3b0c6112a5fcd4d1c42
l3acon <l3acon@gmail.com>
2014-01-25 18:50:39 -0700

    added help page

--- src/main.c
+++ src/main.c
@@ -13,6 +13,22 @@
 #include "departments.h"
 #include "gamestate.h"
 
+void PrintManual()
+{
+  printf("%-15s%-25s%-25s%-25s\n", "department", "example command", "notes", "base delay");
+  
+  printf("\n%-15s%-25s%-25s%-25s\n", "helm", "rotate clockwise 12", "rotates ship to heading", "1 tick per 15 degrees");
+  printf("%-15s%-25s%-25s%-25s\n", "",     "degrees",             "of 12 degrees in the",                "");
+  printf("%-15s%-25s%-25s%-25s\n", "",     "",                    "clockwise direction ",                "");
+
+  printf("\n%-15s%-25s%-25s%-25s\n", "sonar", "list contacts", "lists enemies", "1 tick ");
+
+  printf("\n%-15s%-25s%-25s%-25s\n", "weapons", "load", "loads torpedo", "2 ticks");
+  printf("\n%-15s%-25s%-25s%-25s\n", "",          "fire", "fires torpedo", "1 tick per 20 meters");
+
+
+}
+
 void RunSub()
 { 
   char buffer[MAX_BUFFER_LENGTH];
@@ -33,6 +49,8 @@ void RunSub()
       {
 	running = false;
       }
+      else if(!strcmp(buffer,"manual") || !strcmp(buffer,"help"))
+          PrintManual();
       else if(!strcmp(buffer, "pause"))
       {
 	GameState_Pause(&state);
@@ -44,6 +62,8 @@ void RunSub()
     }
   }
 }
+
+
 
 int main(int argc, char** argv)
 {

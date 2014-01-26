53a2923d08af4c663c2f5b7443cc452374e792ac
l3acon <l3acon@gmail.com>
2014-01-25 21:03:45 -0700

    greened weaponse

--- src/weapons.c
+++ src/weapons.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "weapons.h"
 
+#define COLOR_GREEN   "\033[32m"      /* Green */
+#define COLOR_RESET   "\033[0m"
+
+
 /* Returns the index of the first 'type'd torpedo, or -1 if there isn't one. */
 s32 FirstTorpedoOfType(GameState* state, TorpedoState type)
 {
@@ -36,18 +40,18 @@ void Weapons_PrepTorpedo(GameState* state)
 {
   if(TorpedoPrepped(state) != -1)
   {
-    printf("[WEAPONS]: Prep torpedo failed; Torpedo already being prepped.\n");
+    printf(COLOR_GREEN "[WEAPONS]: Prep torpedo failed; Torpedo already being prepped." COLOR_RESET"\n");
   }
   else
   {
     s32 storeIdx = TorpedoStorage(state);
     if(storeIdx == -1)
     {
-      printf("[WEAPONS]: Prep torpedo failed; No torpedos in storage.\n");
+      printf(COLOR_GREEN "[WEAPONS]: Prep torpedo failed; No torpedos in storage." COLOR_RESET "\n");
     }
     else
     {
-      printf("[WEAPONS]: Torpedo prepped. Ready for upgrading or loading.\n");
+      printf(COLOR_GREEN "[WEAPONS]: Torpedo prepped. Ready for upgrading or loading." COLOR_RESET "\n");
       Torpedo_SetState(state->torpedos + storeIdx, TORPEDOSTATE_PREP);
     }
   }
@@ -57,19 +61,19 @@ void Weapons_LoadTorpedo(GameState* state)
 {
   if(TorpedoLoaded(state) != -1)
   {
-    printf("[WEAPONS]: Load torpedo failed; Torpedo already loaded.\n");
+    printf(COLOR_GREEN "[WEAPONS]: Load torpedo failed; Torpedo already loaded." COLOR_RESET "\n");
   }
   else
   {
     s32 prepped = TorpedoPrepped(state);
     if(prepped == -1)
     {
-      printf("[WEAPONS]: Load torpedo failed; No prepped torpedo. use {torpedo prep} first.\n");
+      printf(COLOR_GREEN "[WEAPONS]: Load torpedo failed; No prepped torpedo. use {torpedo prep} first." COLOR_RESET "\n");
     }
     else
     {
       Torpedo t = state->torpedos[prepped];
-      printf("[WEAPONS]: %s%s%s torpedo loaded.\n", 
+      printf(COLOR_GREEN "[WEAPONS]: %s%s%s torpedo loaded." COLOR_RESET "\n", 
 	     t.booster ? "booster " : "",
 	     t.warhead ? "warhead " : "",
 	     t.homing  ? "homing "  : "");
